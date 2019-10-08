385d8e4527cf6119fcde2ce5edc00fb2fb90457e
Nehemias Vicente <nvnehemias@gmail.com>
2019-10-07 19:58:26 -0700

    Added space for charts

--- flask_boilerplate/templates/Test.js
+++ flask_boilerplate/templates/Test.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
       var hash = this.hash;
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
+      }, 900, function(){
         window.location.hash = hash;
       });
     } 
